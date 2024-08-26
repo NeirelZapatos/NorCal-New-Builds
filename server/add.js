543e8357c1a3add1a2002344aaf717a504d43b1e
Neirel <neirelzapatos@gmail.com>
2024-08-26 13:10:37 -0700

    addBeazer added

--- server/add.js
+++ server/add.js
@@ -15,6 +15,7 @@ const db = new pg.Pool({
 });
 
 async function addJmc() {
+    console.log("Adding JMC");
     try {
         const existingAddressResult = await db.query("SELECT address FROM homes WHERE builder = 'JMC Homes'");
         const existingAddresses = new Set(existingAddressResult.rows.map(row => row.address));
@@ -28,7 +29,7 @@ async function addJmc() {
             const status = $(element).find(".HomeCard_media .HomeCard_statusWrapper .HomeCard_status").text();
 
             if (status === "move-in ready") {
-                const city = $(element).find(".HomeCard_city").text();
+                const city = $(element).find(".HomeCard_city").text().split(",")[0].trim();
                 const address = $(element).find(".HomeCard_address a").text().replace(city, "").trim();
                 currentAddresses.add(address);
 
@@ -70,6 +71,7 @@ async function addJmc() {
 }
 
 async function addWoodside() {
+    console.log("Adding Woodside");
     try {
         const existingAddressResult = await db.query("SELECT address FROM homes WHERE builder = 'Woodside Homes'");
         const existingAddresses = new Set(existingAddressResult.rows.map(row => row.address));
@@ -113,144 +115,57 @@ async function addWoodside() {
 }
 
 async function addBeazer() {
+    console.log("Adding Beazer");
     try {
         const existingAddressResult = await db.query("SELECT address FROM homes WHERE builder = 'Beazer Homes'");
         const existingAddresses = new Set(existingAddressResult.rows.map(row => row.address));
-        const currentAddresses = new Set();
-    
-        const browser = await puppeteer.launch({ 
-            headless: false,
-            defaultViewport: {
-                width: 1280,
-                height: 800
-            }
-        });
-        const page = await browser.newPage();
-    
-        await page.goto("https://www.beazer.com/search-ca-sacramento");
-        await page.waitForSelector('a'); // Wait for any anchor tag to be available
-        await page.evaluate(() => {
-            const links = Array.from(document.querySelectorAll('a'));
-            const quickMoveInsLink = links.find(link => link.textContent.includes('Quick Move-ins'));
-            if (quickMoveInsLink) {
-                quickMoveInsLink.click();
-            }
-        });
-    
-        await page.waitForSelector('div.product-card.card_spec_outer.horizontal');
-    
-        // Step 1: Select all product-cards
-        const houseCards = await page.$$('div.product-card.card_spec_outer.horizontal');
-    
-        // Step 2: Iterate through each product card
-        for (const house of houseCards) {
-            // Step 3: Find the bold element with classes 'bold block uppercase'
-            const statusText = await house.$('b.bold.block.uppercase');
-       
-            // Extract the text content
-            const status = await page.evaluate(element => element.textContent, statusText);
-                
-            if (status === "Now") {
-                const link = await house.$eval('a.red-button.margin-medium', anchor => {
-                    return anchor.href;
-                });
-                const segments = link.split("/");
-                const address = segments[6].replace(/-/g, " ");
-                currentAddresses.add(address);
 
-                if (!existingAddresses.has(address)) {
-                    const city = (await house.$eval('.seriesname', locationText => locationText.textContent)).split("|")[1].trim();
-                    const community = (await house.$eval('.seriesname', locationText => locationText.textContent)).split("|")[0].trim();
-                    const price = (await house.$eval('.font18.no-margin.right-align', priceText => priceText.textContent)).replace(/[^0-9]/g, "").trim();
-                }
-            }
-        }
-        const addressesToDelete = Array.from(existingAddresses).filter(address => !currentAddresses.has(address));
-
-        if (addressesToDelete.length > 0) {
-            await db.query("DELETE FROM homes WHERE address = ANY($1)", [addressesToDelete]);
-        }
-    } catch (err) {
-        console.log("Error in addWoodside:", err);
-    }  
-}
-
-// gonna need to use pupeteer dynamic web page
-async function addCentury() {
-    try {
-        const existingAddressResult = await db.query("SELECT address FROM homes WHERE builder = 'Woodside Homes'");
-        const existingAddresses = new Set(existingAddressResult.rows.map(row => row.address));
-
-        const response = await axios.get("https://www.centurycommunities.com/find-your-new-home/california/northern-california-metro/");
+        const response = await axios.get("https://www.beazer.com/search-ca-sacramento");
         const $ = cheerio.load(response.data);
 
         const currentAddresses = new Set();
 
-        const homePromises = $(".century-card").map(async (index, element) => {
-            const address = $(element).find(".panel-home__content-description").text().trim();
+        const homePromises = $(".product-card.card_spec_outer").map(async (index, element) => {
+            const status = $(element).find(".stats .bold.block.uppercase").text().trim();
+            const address = $(element).find("a").attr("href").split("/")[4].replace(/-/g, " ");
             currentAddresses.add(address);
 
-            if (!existingAddresses.has(address)) {
+            if (status === "Now" && !existingAddresses.has(address)) {
+                const city = $(element).find(".seriesname").text().split("|")[1].split(",")[0].trim();
+                const community = $(element).find(".seriesname").text().split(" | ")[0].trim();
+                const price = $(element).find(".info .font18.no-margin.right-align").text().trim().replace(/[^0-9]/g, "");
+                const sqFt = $(element).find(".stats ul li").eq(5).text().trim().replace(/[^0-9]/g, "");
+                const beds = $(element).find(".stats ul li").eq(3).text().trim().replace(/[^0-9]/g, "");
+                const baths = $(element).find(".stats ul li").eq(4).text().trim().replace(/[^0-9.]/g, "");
+                const garages = -1;
+                const imgUrl = $(element).find("img").attr("src");
 
                 await db.query(`
                     INSERT INTO homes (builder, address, city, community, price, sqft, beds, baths, garages, imgurl)
                     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
-                    ["Century Homes", address, city, community, price, sqFt, beds, baths, garages, imgUrl]
+                    ["Beazer Homes", address, city, community, price, sqFt, beds, baths, garages, imgUrl]
                 );
             }
         }).get();
-        
         await Promise.all(homePromises);
-        const addressesToDelete = Array.from(existingAddresses).filter(address => !currentAddresses.has(address));
 
+        const addressesToDelete = Array.from(existingAddresses).filter(address => !currentAddresses.has(address));
         if (addressesToDelete.length > 0) {
             await db.query("DELETE FROM homes WHERE address = ANY($1)", [addressesToDelete]);
         }
     } catch (err) {
-        console.log("Error in addCentury:", err);
+        console.log("Error in addBeazer:", err);
     }
-    console.log("Finished adding Century Homes");
 }
 
-// gonna need to use pupeteer dynamic web page
-async function addMeritage() {
-    try {
-        const existingAddressResult = await db.query("SELECT address FROM homes WHERE builder = 'Meritage Homes'");
-        const existingAddresses = new Set(existingAddressResult.rows.map(row => row.address));
-
-        const response = await axios.get("https://www.meritagehomes.com/state/ca/sacramento/search/qmi/1");
-        const $ = cheerio.load(response.data);
-
-        const currentAddresses = new Set();
-
-        const homePromises = $(".community-horizontal").map(async (index, element) => {
-            const locationInfo = $(element).find(".mid p.community--range-mo").text();
-            console.log(locationInfo);
-            // const address = $(element).find(".community--range-mo").text().trim();
-            // currentAddresses.add(address);
-
-            // if (!existingAddresses.has(address)) {
-
-            //     await db.query(`
-            //         INSERT INTO homes (builder, address, city, community, price, sqft, beds, baths, garages, imgurl)
-            //         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
-            //         ["Meritage Homes", address, city, community, price, sqFt, beds, baths, garages, imgUrl]
-            //     );
-            // }
-        }).get();
-        
-        await Promise.all(homePromises);
-        const addressesToDelete = Array.from(existingAddresses).filter(address => !currentAddresses.has(address));
-
-        if (addressesToDelete.length > 0) {
-            await db.query("DELETE FROM homes WHERE address = ANY($1)", [addressesToDelete]);
-        }
-    } catch (err) {
-        console.log("Error in addMeritage:", err);
-    }
-    console.log("Finished adding Meritage Homes");    
+async function addTripointe() {
+    
+}
+ 
+function addHomes() {
+    addJmc();
+    addWoodside();
+    addBeazer();
 }
 
-addBeazer();
-
-export { addJmc, addWoodside }
+export default addHomes;
